a45277a3987d40ff3e1276cbfe4df57b7ef7c815
Mythrex <shivam.kaushik73@gmail.com>
2018-06-05 14:57:39 +0530

    Setup CasperJs and Did some work

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const fs = require('fs');
-const cheerio = require('cheerio');
-const request = require('request');
+var casper = require('casper').create();
+var request = require('request')
 
-request('https://www.instagram.com/prisma/?hl=en',function (err, response, html) {
-  var $ = cheerio.load(html)
+var aLinks;
 
-  
+const URL = 'https://www.instagram.com'
+function getALinks(){
+  var links = document.querySelectorAll('.v9tJq article div > div > .Nnq7C .v1Nh3 a')
+  return Array.prototype.map.call(links, function (elem) {
+    return elem.getAttribute('href')
+  })
+}
+
+// function getImgLinks(){
+//     var links = document.querySelectorAll('.v9tJq article div > div > .Nnq7C img')
+//     return Array.prototype.map.call(links, function (elem) {
+//       return elem.getAttribute('src')
+//     })
+// }
+
+casper.start(`${URL}/prisma/?hl=en`);
+
+casper.then(function () {
+  aLinks = this.evaluate(getALinks);
+  this.echo(aLinks)
 })
+
+// casper.then(function() {
+//     imgLinks = this.evaluate(getImgLinks)
+//     this.echo(imgLinks)
+// });
+
+casper.run();

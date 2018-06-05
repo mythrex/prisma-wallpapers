5c424e0295ba42b687cb88253162bfde83d25f2a
Mythrex <shivam.kaushik73@gmail.com>
2018-06-05 20:07:09 +0530

    Second Step added!

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 var casper = require('casper').create();
-var request = require('request')
+var http = require('http')
 
 var aLinks, imgLink;
 var randomInt = 0;
-var IMAGE_URL = 'https://www.instagram.com/prisma/'
+var IMAGE_URL = 'https://www.instagram.com'
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -12,35 +12,45 @@ function getRandomInt(max) {
 
 function getALinks(){
   var links = document.querySelectorAll('.v9tJq article div div .Nnq7C .v1Nh3 a')
+  if(!links.length) getALinks();
   return Array.prototype.map.call(links, function (elem) {
     return elem.getAttribute('href')
   })
 }
 
 function getImgLink(){
-  var img = document.querySelectorAll('.zZYga .PdwC2 article .FFVAD');
-  return (Array.prototype.map.call(img, function (elem) {
-    return elem.getAttribute('src')
-  }))[0]
+  var img = document.querySelectorAll('article.QBXjJ .eLAPa .KL4Bh .FFVAD');
+  return img[0].getAttribute('src')
+  // if(img.length){
+  //   return Array.prototype.map.call(img, function (elem) {
+  //     return elem.getAttribute('src')
+  //   })
+  // }
+  // getImgLink();
 }
 
 casper.start('https://www.instagram.com/prisma/?hl=en');
 
 casper.then(function () {
   aLinks = this.evaluate(getALinks)
-  this.echo(typeof(aLinks[]));
   //assign a random No from 0 - aLinks.length
   randomInt = getRandomInt(aLinks.length)
+  this.echo(randomInt)
+  this.echo(aLinks)
+  IMAGE_URL = IMAGE_URL+aLinks[randomInt]
   this.echo('Downloading to '+(randomInt + 1) + ' image')
-  // IMAGE_URL = IMAGE_URL + '/' +
 })
 
-// casper.thenOpen(IMAGE_URL+aLinks[randomInt], function () {
-//   this.echo('Seaching the Wallpaper...')
-//   imgLink = this.evaluate(getImgLink)
-//   this.echo('Image Link' + imgLink)
-//   this.echo('Downloading Wallpaper Please wait..')
-//   request(imgLink).pipe(fs.createWriteStream('Wallpaper.jpg'))
-// })
+casper.thenOpen(IMAGE_URL, function () {
+  this.echo('Opening Url: '+IMAGE_URL)
+  this.echo('Seaching the Wallpaper...')
+  // imgLink = this.evaluate(getImgLink)
+  this.echo(imgLink)
+  if(imgLink){
+    this.echo('Downloading Wallpaper Please wait..')
+    this.download(imgLink,'wallpaper.jpg');
+  }
+})
+
 
 casper.run();
